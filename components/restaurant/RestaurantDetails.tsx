5b02da236142ce3424a31e95bc05b977f9b4751f
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2024-08-09 21:10:40 +0530

    ui changes in restaurant profile page

--- components/restaurant/RestaurantDetails.tsx
+++ components/restaurant/RestaurantDetails.tsx
@@ -1,7 +1,21 @@
+import { ExternalLink } from "lucide-react";
 import React from "react";
 
 const RestaurantDetails = () => {
-  return <div>RestaurantDetails</div>;
+  return (
+    <div className="px-4 w-full h-full">
+      <div className="flex flex-row items-center justify-between">
+        <p className="font-semibold">Edit Profile</p>
+        <p>
+          <ExternalLink
+            className="text-bittersweet-500 cursor-pointer"
+            size={22}
+          />
+        </p>
+      </div>
+      <div className="grid md:grid-cols-2 grid-cols-1"></div>
+    </div>
+  );
 };
 
 export default RestaurantDetails;
