437d692743b4a6d2c51c398c1e2967d2570f7acc
GameOfCode64 <131631135+GameOfCode64@users.noreply.github.com>
2024-09-01 00:13:04 +0530

    ui update in landing page

--- components/TopRestaurant.tsx
+++ components/TopRestaurant.tsx
@@ -4,7 +4,7 @@ import getallRestaurant from "@/actions/getallRestaurant";
 
 const TopRestaurant = async () => {
   return (
-    <div className="w-full md:h-[250px] h-[300px] ">
+    <div className="w-full md:h-[250px] h-[300px] mb-32">
       <RestaurantCard />
     </div>
   );
